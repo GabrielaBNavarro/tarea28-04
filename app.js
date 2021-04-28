b6a68059c2cd65ffb3e269895e38b460eeda8348
Gabriela Navarro <gabiluu.n@gmail.com>
2021-04-28 18:18:21 -0300

    Tarea terminada

---
+++ app.js
@@ -0,0 +1,22 @@
+let numero1 =10
+let numero2 = 25
+console.log("Numero 1", numero1)
+console.log("Numero 2", numero2)
+//Suma
+console.log("Suma")
+console.log(numero1 + numero2)
+//Resta
+console.log("Resta")
+console.log(numero2 - numero1)
+//Multiplicacion
+console.log("Multiplicacion")
+console.log( numero1*numero2)
+//Division
+console.log("Cociente")
+console.log( numero1/numero2)
+//Comparacion
+console.log("Es el primer número mayor o igual que el segundo?")
+console.log(numero1>=numero2)
+// comparacion 2
+console.log("Es el primer número distinto del segundo? ")
+console.log(numero1!=numero2)
